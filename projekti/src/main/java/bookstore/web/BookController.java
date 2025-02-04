c7a0fab98db21fea3f622a0d4b797ac0d9ea1fa6
EetuP22 <bhi219@myy.haaga-helia.fi>
2025-02-04 13:54:59 +0200

    vaihe 3 tehty

--- projekti/src/main/java/bookstore/web/BookController.java
+++ projekti/src/main/java/bookstore/web/BookController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
 
 
@@ -24,5 +26,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/delete{id}")
+    public String deleteBook (@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @GetMapping("/save") 
+    public String save(Book book) {
+        bookRepository.save(book);
+        return "redirect:../booklist";
     }
 }
