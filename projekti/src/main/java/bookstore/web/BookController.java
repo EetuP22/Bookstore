dd7b59981e2a132f9549308a31d9bbd99d979a5e
EetuP22 <bhi219@myy.haaga-helia.fi>
2025-02-06 16:24:02 +0200

    Chapter 4 tehtävät tehty

--- projekti/src/main/java/bookstore/web/BookController.java
+++ projekti/src/main/java/bookstore/web/BookController.java
@@ -7,16 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
-
+import bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/index")
     public String showIndex(Model model) {
@@ -31,21 +36,22 @@ public class BookController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteBook (@PathVariable("id") Long id) {
+    public String deleteBook(@PathVariable("id") Long id) {
         bookRepository.deleteById(id);
-        return "redirect:../booklist";
+        return "redirect:/booklist";
     }
 
     @GetMapping("/add")
     public String addBook(Model model) {
         model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryRepository.findAll()); 
         return "addbook";
     }
 
-    @GetMapping("/save") 
-    public String save(Book book) {
+    @PostMapping("/save") 
+    public String saveBook(@ModelAttribute Book book) {
         bookRepository.save(book);
-        return "redirect:../booklist";
+        return "redirect:/booklist";
     }
 
     @GetMapping("/edit/{id}")
@@ -53,9 +59,10 @@ public class BookController {
         Optional<Book> book = bookRepository.findById(id);
         if (book.isPresent()) {
             model.addAttribute("book", book.get());
+            model.addAttribute("categories", categoryRepository.findAll()); 
             return "editbook";
         } else {
-            return "redirect:../booklist";
+            return "redirect:/booklist";
         }
     }
 }
---
+++ projekti/src/main/java/bookstore/web/ProjektiApplication.java
@@ -0,0 +1,42 @@
+package bookstore.web;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import bookstore.domain.Book;
+import bookstore.domain.BookRepository;
+import bookstore.domain.Category;
+import bookstore.domain.CategoryRepository;
+
+@SpringBootApplication(scanBasePackages = "bookstore")
+@EntityScan("bookstore.domain")
+@EnableJpaRepositories("bookstore.domain")
+public class ProjektiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
+        return (args) -> {
+
+            Category fiction = new Category("Fiction");
+            Category philosophy = new Category("Philosophy");
+            Category history = new Category("History");
+
+            categoryRepository.save(fiction);
+            categoryRepository.save(philosophy);
+            categoryRepository.save(history);
+
+
+            bookRepository.save(new Book("The Book Of Five Rings", "Miyamoto Musashi", 1645, "9781590309841", 9.99, philosophy));
+            bookRepository.save(new Book("Meditations", "Marcus Aurelius", 180, "9780140449334", 12.99, philosophy));
+            bookRepository.save(new Book("Metamorphosis", "Franz Kafka", 1915, "9780140283295", 7.99, fiction));
+        };
+    }
+}
